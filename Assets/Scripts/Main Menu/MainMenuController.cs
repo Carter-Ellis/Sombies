9bc493198e9405a5cba18a755d3db9a92b8ad6a7
Carter Ellis <Carterellis13@gmail.com>
2026-07-22 17:34:29 -0400

    Link FMOD to Unity

--- Assets/Scripts/Main Menu/MainMenuController.cs
+++ Assets/Scripts/Main Menu/MainMenuController.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using FMODUnity;
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private EventReference welcomeSequence;
+
+    private void Awake()
+    {
+        AudioManager.Instance.PlayOneShot(welcomeSequence, Vector3.zero);
+    }
+
     public void StartGame()
     {
         // This loads the next scene in your Build Settings
